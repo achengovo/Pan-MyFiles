0c9c6611516e5c8632984b4149c1f3a99cb85cf4
achengovo <achengovo@qq.com>
2022-05-01 22:53:54 +0800

    解决大文件下载内存爆满问题 解决验证码大小写区分问题（改为不区分）

--- src/main/java/com/yunpan/achengovo/controller/FileController.java
+++ src/main/java/com/yunpan/achengovo/controller/FileController.java
@@ -1,4 +1,5 @@
 package com.yunpan.achengovo.controller;
+
 import com.yunpan.achengovo.entity.User;
 import com.yunpan.achengovo.entity.UserFile;
 import com.yunpan.achengovo.service.FileService;
@@ -6,7 +7,6 @@ import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -27,8 +27,10 @@ import static com.yunpan.achengovo.utils.judgeFileType.judgeType;
 public class FileController {
     @Autowired
     private FileService fileService;
+
     /**
      * 移动文件
+     *
      * @param userFile
      * @param session
      * @return
@@ -36,39 +38,42 @@ public class FileController {
     @ResponseBody
     @RequestMapping(value = "/moveFile")
     public String moveFile(@RequestBody UserFile userFile, HttpSession session) {
-        HashSet hashSet=new HashSet();
-        if(!"".equals(userFile.getDir())){
-            hashSet=getUpDir(Integer.valueOf(userFile.getDir()),hashSet,session);
-            if(hashSet.contains(String.valueOf(userFile.getUserFileId()))){
+        HashSet hashSet = new HashSet();
+        if (!"".equals(userFile.getDir())) {
+            hashSet = getUpDir(Integer.valueOf(userFile.getDir()), hashSet, session);
+            if (hashSet.contains(String.valueOf(userFile.getUserFileId()))) {
                 return "不能将文件夹移动到下级目录";
             }
         }
         User user = (User) session.getAttribute("USER_SESSION");
         userFile.setUserId(user.getUserId());
-        if(fileService.moveFile(userFile)!=0){
+        if (fileService.moveFile(userFile) != 0) {
             return "success";
-        }else {
+        } else {
             return "移动失败";
         }
     }
+
     //递归获取上级目录
-    public HashSet getUpDir(Integer userFileId,HashSet hashSet,HttpSession session){
+    public HashSet getUpDir(Integer userFileId, HashSet hashSet, HttpSession session) {
         hashSet.add(String.valueOf(userFileId));
         User user = (User) session.getAttribute("USER_SESSION");
-        UserFile userFile=new UserFile();
+        UserFile userFile = new UserFile();
         userFile.setUserId(user.getUserId());
         userFile.setUserFileId(userFileId);
-        userFile=fileService.getFileByUserFileId(userFile);
-        if(userFile!=null){
-            if(!"".equals(userFile.getDir())){
+        userFile = fileService.getFileByUserFileId(userFile);
+        if (userFile != null) {
+            if (!"".equals(userFile.getDir())) {
                 hashSet.add(userFile.getDir());
-                getUpDir(Integer.valueOf(userFile.getDir()),hashSet,session);
+                getUpDir(Integer.valueOf(userFile.getDir()), hashSet, session);
             }
         }
         return hashSet;
     }
+
     /**
      * 重命名
+     *
      * @param userFile
      * @param request
      * @param session
@@ -79,14 +84,16 @@ public class FileController {
     public String reName(@RequestBody UserFile userFile, HttpServletRequest request, HttpSession session) {
         User user = (User) session.getAttribute("USER_SESSION");
         userFile.setUserId(user.getUserId());
-        if (fileService.reName(userFile)!=0) {
+        if (fileService.reName(userFile) != 0) {
             return "success";
         } else {
             return "重命名失败";
         }
     }
+
     /**
      * 删除
+     *
      * @param userFile
      * @param request
      * @param session
@@ -95,25 +102,27 @@ public class FileController {
     @ResponseBody
     @RequestMapping(value = "/delFileById")
     public String delFileById(@RequestBody UserFile userFile, HttpServletRequest request, HttpSession session) {
-        if (delFileByIdType(userFile,session)) {
+        if (delFileByIdType(userFile, session)) {
             return "success";
         } else {
             return "删除失败";
         }
     }
+
     //递归删除
-    public boolean delFileByIdType(UserFile userFile,HttpSession session) {
+    public boolean delFileByIdType(UserFile userFile, HttpSession session) {
         User user = (User) session.getAttribute("USER_SESSION");
         userFile.setUserId(user.getUserId());
         userFile = fileService.getFileByUserFileId(userFile);
         if (fileService.delFileById(userFile) != 0) {
-            if("dir".equals(userFile.getFileType())){
+            if ("dir".equals(userFile.getFileType())) {
                 userFile.setDir(String.valueOf(userFile.getUserFileId()));
-                List<UserFile> userFileList=fileService.getFileByDir(userFile);
-                for (UserFile file:userFileList) {
-                    if(delFileByIdType(file,session)){
+                List<UserFile> userFileList = fileService.getFileByDir(userFile);
+                for (UserFile file : userFileList) {
+                    if (delFileByIdType(file, session)) {
                         continue;
-                    };
+                    }
+                    ;
                 }
             }
         }
@@ -130,6 +139,7 @@ public class FileController {
         System.out.println(userFile);
         return fileService.getFileByDir(userFile);
     }
+
     @ResponseBody
     @RequestMapping("/view")
     public ResponseEntity<byte[]> view(@RequestParam("userFileId") Integer userFileId, HttpServletRequest request, HttpSession session, HttpServletResponse response) throws IOException {
@@ -148,8 +158,9 @@ public class FileController {
         return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.OK);
 
     }
+
     @RequestMapping("/download")
-    public ResponseEntity<byte[]> download(@RequestParam("userFileId") Integer userFileId, HttpServletRequest request, HttpSession session) throws IOException {
+    public void download(@RequestParam("userFileId") Integer userFileId, HttpServletRequest request, HttpSession session, HttpServletResponse response) throws IOException {
         User user = (User) session.getAttribute("USER_SESSION");
         UserFile userFile = new UserFile();
         userFile.setUserFileId(userFileId);
@@ -158,11 +169,27 @@ public class FileController {
         userFile1 = fileService.getFileByUserFileId(userFile);
 //        String path="D://桌面/upload/"+userFile1.getFileLocation();
         String path = request.getServletContext().getRealPath("/upload/");
-        File file = new File(path + File.separator + userFile1.getFileLocation());
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentDispositionFormData("attachment", this.getEncodeName(request, userFile1.getUserFileName()));
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.OK);
+//        File file = new File(path + File.separator + userFile1.getFileLocation());
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentDispositionFormData("attachment", this.getEncodeName(request, userFile1.getUserFileName()));
+//        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+//        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.OK);
+
+
+        String filePath = path + File.separator + userFile1.getFileLocation();
+        String fileName = URLEncoder.encode(userFile1.getUserFileName().trim(), "UTF-8");
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+        InputStream is = new FileInputStream(filePath);
+
+        int read = 0;
+        byte[] bytes = new byte[200 * 1024];
+        OutputStream os = response.getOutputStream();
+        while ((read = is.read(bytes)) != -1) {//按字节逐个写入，避免内存占用过高
+            os.write(bytes, 0, read);
+        }
+        os.flush();
+        os.close();
+        is.close();
     }
 
     /**
@@ -178,31 +205,33 @@ public class FileController {
         }
         return new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
     }
+
     /**
      * 上传文件
+     *
      * @param uploadfile
      * @param request
      * @return
      */
     @ResponseBody
-    @RequestMapping(value = "/toUpload",method = RequestMethod.POST)
+    @RequestMapping(value = "/toUpload", method = RequestMethod.POST)
     public String upload(
             @RequestParam("uploadfile") List<MultipartFile> uploadfile,
             @RequestParam("nowDirId") String nowDirId,
             HttpServletRequest request, HttpSession session) {
-        User user= (User) session.getAttribute("USER_SESSION");
+        User user = (User) session.getAttribute("USER_SESSION");
 //        System.out.println(name);
         System.out.println(nowDirId);
         //判断上传文件是否存在
         if (!uploadfile.isEmpty() && uploadfile.size() > 0) {
-            String newFilename="";
+            String newFilename = "";
             //循环输出上传的文件
             for (MultipartFile file : uploadfile) {
                 //上传文件的原始名称
                 String originalFilename = file.getOriginalFilename();
                 //设置上传文件的保存地址目录
 //                String dirPath = "D://桌面/upload/";
-                String dirPath=request.getServletContext().getRealPath("/upload/");
+                String dirPath = request.getServletContext().getRealPath("/upload/");
                 File filePath = new File(dirPath);
                 //如果保存文件的地址不存在就先创建目录
                 if (!filePath.exists()) {
@@ -212,16 +241,17 @@ public class FileController {
                 newFilename = UUID.randomUUID() + "_" + originalFilename;
                 try {
                     file.transferTo(new File(dirPath + newFilename));
-                    UserFile userFile=new UserFile();
+                    UserFile userFile = new UserFile();
                     userFile.setUserFileName(originalFilename);
                     userFile.setUserId(user.getUserId());
                     userFile.setFileType(judgeType(originalFilename));
                     userFile.setFileLocation(newFilename);
                     userFile.setDir(nowDirId);
                     userFile.setUserFileSize(String.valueOf(file.getSize()));
-                    if(fileService.insertToUserFiles(userFile)==0){
+                    if (fileService.insertToUserFiles(userFile) == 0) {
                         return "error";
-                    };
+                    }
+                    ;
                 } catch (Exception e) {
                     e.printStackTrace();
                     return "error";
@@ -232,6 +262,7 @@ public class FileController {
             return "error";
         }
     }
+
     /**
      * 新建目录
      * fileId, fileType,fileLocation
@@ -239,11 +270,11 @@ public class FileController {
     @ResponseBody
     @RequestMapping("/newDir")
     public String newDir(@RequestBody UserFile userFile,
-                         HttpSession session){
+                         HttpSession session) {
         System.out.println(userFile);
-        User user= (User) session.getAttribute("USER_SESSION");
+        User user = (User) session.getAttribute("USER_SESSION");
         userFile.setUserId(user.getUserId());
-        if(fileService.newDir(userFile)!=0){
+        if (fileService.newDir(userFile) != 0) {
             return "success";
         }
         return "创建失败";
